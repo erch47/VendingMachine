355472a1ac9603fa505f36a259f6811e93c145ee
erch47 <erik.chr1@hotmail.se>
2022-10-08 14:11:53 +0200

    Add project files.

---
+++ VendingMachine/Models/Drink.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine.Models
+{
+    public class Drink : Product
+    {
+        public override string Examine()
+        {
+
+        }
+
+        public override void Use()
+        {
+
+        }
+    }
+}
---
+++ VendingMachine/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine.Models
+{
+    public abstract class Product
+    {
+        public abstract string Examine();
+        public abstract void Use();
+
+    }
+}
